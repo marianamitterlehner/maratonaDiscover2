256966765273cb13af3a584c42e86777bc92b6fc
Mariana Mitterlehner <mariana.m.miguel@gmail.com>
2021-04-18 16:36:09 -0300

    banco parte 4

--- src/db/config.js
+++ src/db/config.js
@@ -1,14 +1,15 @@
-const sqlite3 = require('sqlite3');
-const { open } = require('sqlite')  /**{} permite trazer uma unica funcionalidade do pacote sem ter que importar o pacote interio */
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");  /**{} permite trazer uma unica funcionalidade do pacote sem ter que importar o pacote interio */
 
 /**abrindo a conexao com o banco de dados */
 
-module.exports = () => {
+module.exports = () => 
     open({
-        filename: './database.sqlite',
-        driver: 'sqlite3.Database'  /**executavel do sqlite */
+        filename: "./database.sqlite",
+        driver: sqlite3.Database  /**executavel do sqlite */
     }); 
-}
+
 
 /**arrow fuction => fala que e uma estrutura de funcao e nao um objeto entre chaves */
 /**module exports ativa automaticamente o open */
+/**quando e so um item dentro da => nao precisa de {} */
--- src/db/init.js
+++ src/db/init.js
@@ -1,77 +1,88 @@
-const Database = require('config');
+const Database = require('./config');
 
-Database()
+const initDb = {
 
-Database.exec(`
-    CREATE TABLE profiles (
-        id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        name TEXT, 
-        avatar TEXT, 
-        monthly_budget INT, 
-        days_per_week INT, 
-        hours_per_day INT, 
-        vocation_per_year INT, 
-        value_hours INT
-    );
-`)
-
-Database.exec(`
-    CREATE TABLE jobs (
-        id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        name TEXT, 
-        daily_hours INT, 
-        total_hours INT,
-        created_at DATETIME
-    );
-`)
-
-Database.run(`
-    INSERT INTO profiles (
-        name,
-        avatar, 
-        monthly_budget, 
-        days_per_week, 
-        hours_per_day, 
-        vocation_per_year
-    ) VALUES (
-        "Mariana",
-        "https://avatars.githubusercontent.com/u/51057747?v=4",
-        358521,
-        54,
-        2,
-        8
-    ); 
-`);
+    async init(){
+        const db = await Database();
 
 
-Database.run(`
-    INSERT INTO jobs (
-        name,
-        daily_hours,
-        total_hours,
-        created_at
-    ) VALUES (
-        "Pizzaria Guloso",
-        2,
-        2, 
-        1618365183780
-    ); 
+        await db.exec(`
+            CREATE TABLE profiles (
+                id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                name TEXT, 
+                avatar TEXT, 
+                monthly_budget INT, 
+                days_per_week INT, 
+                hours_per_day INT, 
+                vacation_per_year INT, 
+                value_hours INT
+            );
+        `);
+        
+        await db.exec(`
+            CREATE TABLE jobs (
+                id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                name TEXT, 
+                daily_hours INT, 
+                total_hours INT,
+                created_at DATETIME
+            );
+        `);
+        
+        await db.run(`
+            INSERT INTO profiles (
+                name,
+                avatar, 
+                monthly_budget, 
+                days_per_week, 
+                hours_per_day, 
+                vacation_per_year
+            ) VALUES (
+                "Mariana",
+                "https://avatars.githubusercontent.com/u/51057747?v=4",
+                358521,
+                54,
+                2,
+                8
+            ); 
+        `);
+        
+        
+        await db.run(`
+            INSERT INTO jobs (
+                name,
+                daily_hours,
+                total_hours,
+                created_at
+            ) VALUES (
+                "Pizzaria Guloso",
+                2,
+                2, 
+                1618365183780
+            ); 
+        
+        `);
+        
+        await db.run(`
+            INSERT INTO jobs (
+                name,
+                daily_hours,
+                total_hours,
+                created_at
+            ) VALUES (
+                "OneTwo Project",
+                7,
+                64, 
+                1618365183780
+            ); 
+            
+        `);
+        
+        await db.close();
+    }
 
-`);
+}
 
-Database.run(`
-    INSERT INTO jobs (
-        name,
-        daily_hours,
-        total_hours,
-        created_at
-    ) VALUES (
-        "OneTwo Project",
-        7,
-        64, 
-        1618365183780
-    ); 
-    
-`);
+initDb.init();
 
-Database.close();
+/** executando o arquivo */
